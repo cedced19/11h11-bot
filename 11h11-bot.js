e45505b8db9976a3dfc0a0766b75e43d3d503e6a
Cédric JUNG <cedced19@gmail.com>
2017-06-25 23:15:55 +0200

    Fix bug when hours < 10

--- 11h11-bot.js
+++ 11h11-bot.js
@@ -2,10 +2,17 @@ const login = require('facebook-chat-api');
 const schedule = require('node-schedule');
 const config = require('./config.json');
 
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
 var send = schedule.scheduleJob('0 0-23 * * * *', function(){
 	var d = new Date();
-	var h = d.getHours();
-	var m = d.getMinutes();
+	var h = addZero(d.getHours());
+	var m = addZero(d.getMinutes());
 	if (m == h) {
 		login(config, (err, api) => {
 		    if(err) return console.error(err);
